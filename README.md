92cccb8e7f3146f018e00eaca196558daab92afc
Alex Gondek <alexgondek@gmail.com>
2015-06-09 13:52:04 -0700

    Improved linkage between API and CLI by refining configuration method

--- README.md
+++ README.md
@@ -7,16 +7,39 @@ Originally built for opening dashboards on TV-connected Raspberry Pis.
 
 ## Usage
 
-Running `./kiosk` will notify you of any missing dependencies. Otherwise, it will display usage help.
+Kiosk Controller requires:
+
+- `chromium-browser`
+- `python3`
+- `bottle.py` library
+
+Running `./kiosk` will notify you of missing dependencies and how to fix them.
+
+Otherwise, it will display usage help.
 
 ## HTTP API
 
-...
+By default, kiosk will start an HTTP API with the following endpoints:
+
+### `GET /` - Current status (HTTP 200)
+
+Returns the plain text of the currently set page.
+
+### `POST /` - Temporarily change page and restart (HTTP 200)
+
+If a HTTP header `kiosk-page` is specified, the page will be temporarily changed and Chromium will restart.
+
+Otherwise, the browser will restart with the default page.
+
+### `PUT /` - Change and persist page and reset (HTTP 200 or HTTP 400)
+
+If a HTTP header `kiosk-page` is specified, the page will be saved as the new default and Chromium will restart.
+
+Otherwise, if no page is specified, and HTTP 400 is returned.
 
 ##Todo
 
-- Fix bug with POST vs PUT in api
-- Add guide for API
-- API-to-shell security improvements
-- Remove hardcoded values from Python (make then args)
+- fix chrome restart failing to close old window
+- Add API auth
 - Code review
+- Remove dependency on bottle?
--- kiosk-http-api.py
+++ kiosk-http-api.py
@@ -1,47 +1,57 @@
 #!/usr/bin/env python3
 
 import subprocess, sys
-from bottle import route, run, request
+from bottle import route, run, request, response
 
-def restart():
-    subprocess.call("./kiosk start", shell=True)
+kiosk_script = sys.argv[1]
+server_port = int(sys.argv[2])
+current_page_file = sys.argv[3]
+saved_page_file = sys.argv[4]
+temp_page_file = sys.argv[5]
 
-def getSite():
-    file = open('data/kiosk_curr', 'r');
+def restartChromium():
+    subprocess.call(kiosk_script + " chromium", shell=True)
+
+def readFile(fileName):
+    file = open(fileName, 'r');
     data = file.read()
     file.close()
     return data
 
-def setSite(newSite, persist=False):
-    oldSite = getSite()
-    file = open('data/kiosk_page', 'w');
-    file.write(newSite);
+def overwriteFile(data, filename):
+    file = open(filename, 'w');
+    file.write(data);
     file.close();
-    restart()
-    if not (persist):
-        file = open('data/kiosk_page', 'w');
-        file.write(oldSite);
-        file.close();
-    return newSite
+
+def getPage():
+    response.content_type = "text/plain"
+    return readFile(current_page_file)
+
+def setTempPage(url):
+    overwriteFile(url, temp_page_file)
+
+def setPage(url):
+    overwriteFile(url, saved_page_file)
 
 @route('/', method='GET')
 def status():
-    return getSite()
+    return getPage()
 
 @route('/', method='POST')
 def status():
-    setSite(request.get_header('kiosk-page', ''))
-    return getSite()
+    newpage = request.get_header('kiosk-page', '')
+    if len(newpage) > 0:
+        setTempPage(newpage)
+    restartChromium()
+    return getPage()
 
 @route('/', method='PUT')
 def status():
-    setSite(request.get_header('kiosk-page', ''), True)
-    return getSite()
+    newpage = request.get_header('kiosk-page', '')
+    if len(newpage) > 0:
+        setPage(newpage)
+        restartChromium()
+        return getPage()
+    response.status = "400 No page specified"
 
-
-try:
-    port = int(sys.argv[1])
-except (ValueError, IndexError):
-    port = 8000
-
-run(host='localhost', port=port)
+run(host='localhost', port=server_port)
